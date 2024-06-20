c0f3f5e9911d69b7be2c539646d388f599170f6b
goshaaaa777 <isaiasddx322z@gmail.com>
2024-06-20 01:38:43 -0500

    first commit

---
+++ src/main/java/com/service/mr/student/api_rest_student_service/ApiRestStudentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.service.mr.student.api_rest_student_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestStudentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestStudentServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/service/mr/student/api_rest_student_service/ApiRestStudentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.mr.student.api_rest_student_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestStudentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
